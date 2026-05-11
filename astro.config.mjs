bb14267ffb9f4f5c38f772f14c8449cc1a97dfe2
Wesley Ford <wes@wesdevpro.com>
2026-05-11 12:20:02 -0400

    chore: batman🦇

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+   alias: {
+      '@assets': 'src/assets',
+   },
+   integrations: [mdx()],
+   vite: {
+      plugins: [tailwindcss()],
+   }
+});
