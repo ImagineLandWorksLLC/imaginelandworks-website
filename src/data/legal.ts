b1b9655b88acf7420eb56c2e0e110cc5b987e40e
Wesley Ford <wes@wesdevpro.com>
2026-06-22 21:43:49 -0400

    chore: abstract legal contact info

---
+++ src/data/legal.ts
@@ -0,0 +1,10 @@
+export const legalContact = {
+  name: "Imagine Land Works",
+  legalName: "Imagine Land Works, LLC",
+  location: "Lakeland, Florida",
+  email: "seth@imaginelandworks.com",
+  phone: {
+    display: "(863) 712-4867",
+    e164: "+18637124867",
+  },
+};
